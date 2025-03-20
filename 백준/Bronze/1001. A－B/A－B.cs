702f44cfeda8f02dc388a0462745eb846868aa70
DevNap <thdwodh789@gmail.com>
2025-03-20 16:59:25 +0900

    [Bronze V] Title: A-B, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,16 @@
+namespace backjun_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] userInput = input.Split();
+
+            int a = int.Parse(userInput[0]);
+            int b = int.Parse(userInput[1]);
+
+            Console.WriteLine(a - b);
+        }
+    }
+}
