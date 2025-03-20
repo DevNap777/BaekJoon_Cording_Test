b39c625d798720a7058a383fd7b54c0a8b439c49
DevNap <thdwodh789@gmail.com>
2025-03-20 18:18:44 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,16 @@
+using System.Data;
+
+namespace backjun_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+        }
+    }
+}
